488c581b8be491e302c06576417126ca0f29f5c7
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2025-07-06 16:37:04 +0100

    Add test with FirefoxDriver

--- DotNetSelenium/UnitTest1.cs
+++ DotNetSelenium/UnitTest1.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace DotNetSelenium
 {
@@ -31,6 +32,20 @@ namespace DotNetSelenium
             // Verify the logout was successful
             Assert.That(driver.FindElement(By.Id("flash")).Text, Does.Contain("You logged out of the secure area!"));
             // Close the browser
+            driver.Quit();        
+        }
+        [Test]
+        public void TestEAWebsite()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Id("UserName")).SendKeys("admin");
+            driver.FindElement(By.Id("Password")).SendKeys("password");
+            driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+            Assert.That(driver.FindElement(By.CssSelector("a[href='/Manage']")).Text, Does.Contain("Hello admin!"));
+            driver.FindElement(By.LinkText("Log off")).Click();
             driver.Quit();
         }
     }
