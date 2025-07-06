44ce18196117ddccc6144d54a2ae4bbe7693b68b
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2025-07-06 17:53:01 +0100

    Add custom methods class

---
+++ DotNetSelenium/SeleniumCustomMethods.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DotNetSelenium
+{
+    internal class SeleniumCustomMethods
+    {
+        public static void Click(IWebDriver driver, By locator)
+        {
+            driver.FindElement(locator).Click();
+        }
+
+        public static void EnterText(IWebDriver driver, By locator, string text)
+        {
+            driver.FindElement(locator).Clear();
+            driver.FindElement(locator).SendKeys(text);
+        }
+
+        public static void SelectDropdownByText(IWebDriver driver, By locator, string text)
+        {
+            SelectElement dropdown = new SelectElement(driver.FindElement(locator));
+            dropdown.SelectByText(text);
+        }
+    }
+}
