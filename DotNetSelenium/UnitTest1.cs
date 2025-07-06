2418dbc5f8133d8ae919e8296dd6fb84c122bc85
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2025-07-06 16:07:49 +0100

    Add TestLoginLogout()

--- DotNetSelenium/UnitTest1.cs
+++ DotNetSelenium/UnitTest1.cs
@@ -1,3 +1,6 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace DotNetSelenium
 {
     public class Tests
@@ -8,9 +11,27 @@ namespace DotNetSelenium
         }
 
         [Test]
-        public void Test1()
+        public void TestLoginLogout()
         {
-            Assert.Pass();
+            // Create a new instance of the ChromeDriver
+            IWebDriver driver = new ChromeDriver();
+            // Navigate to website
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+            // Maximize the browser window
+            driver.Manage().Window.Maximize();
+            // Find elements on the page and fill data
+            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
+            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            // Click the login button
+            driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+            // Verify the login was successful
+            Assert.That(driver.FindElement(By.Id("flash")).Text, Does.Contain("You logged into a secure area!"));
+            // Logout
+            driver.FindElement(By.CssSelector("a[href='/logout']")).Click();
+            // Verify the logout was successful
+            Assert.That(driver.FindElement(By.Id("flash")).Text, Does.Contain("You logged out of the secure area!"));
+            // Close the browser
+            driver.Quit();
         }
     }
 }
