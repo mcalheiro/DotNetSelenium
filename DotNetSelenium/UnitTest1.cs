a900094f2d6c0578394caebae4ca2c5252030300
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2025-07-06 20:07:20 +0100

    Add setup and teardown attributes

--- DotNetSelenium/UnitTest1.cs
+++ DotNetSelenium/UnitTest1.cs
@@ -7,59 +7,29 @@ namespace DotNetSelenium
 {
     public class Tests
     {
+        private IWebDriver _driver;
+
         [SetUp]
         public void Setup()
         {
-        }
-
-        [Test]
-        public void TestLoginLogout()
-        {
-            // Create a new instance of the ChromeDriver
-            IWebDriver driver = new ChromeDriver();
-            // Navigate to website
-            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
-            // Maximize the browser window
-            driver.Manage().Window.Maximize();
-            // Find elements on the page and fill data
-            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
-            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
-            // Click the login button
-            driver.FindElement(By.CssSelector("button[type='submit']")).Click();
-            // Verify the login was successful
-            Assert.That(driver.FindElement(By.Id("flash")).Text, Does.Contain("You logged into a secure area!"));
-            // Logout
-            driver.FindElement(By.CssSelector("a[href='/logout']")).Click();
-            // Verify the logout was successful
-            Assert.That(driver.FindElement(By.Id("flash")).Text, Does.Contain("You logged out of the secure area!"));
-            // Close the browser
-            driver.Quit();
-        }
-
-        [Test]
-        public void TestEAWebsite()
-        {
-            IWebDriver driver = new FirefoxDriver();
-            driver.Navigate().GoToUrl("http://eaapp.somee.com/");
-            driver.Manage().Window.Maximize();
-            driver.FindElement(By.LinkText("Login")).Click();
-            driver.FindElement(By.Id("UserName")).SendKeys("admin");
-            driver.FindElement(By.Id("Password")).SendKeys("password");
-            driver.FindElement(By.CssSelector("input[type='submit']")).Click();
-            Assert.That(driver.FindElement(By.CssSelector("a[href='/Manage']")).Text, Does.Contain("Hello admin!"));
-            driver.FindElement(By.LinkText("Log off")).Click();
-            driver.Quit();
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            _driver.Manage().Window.Maximize();
         }
 
         [Test]
         public void TestWithPOM()
         {
-            IWebDriver driver = new FirefoxDriver();
-            driver.Navigate().GoToUrl("http://eaapp.somee.com/");
-            LoginPage loginPage = new LoginPage(driver);
+            LoginPage loginPage = new LoginPage(_driver);
             loginPage.ClickLoginLink();
             loginPage.Login("admin", "password");
-            driver.Quit();
+            _driver.Quit();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
         }
     }
 }
