a56413b0d4c72a43bd5437ddfcd1b0472e3c2728
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2025-07-06 18:54:38 +0100

    Refactor to use locators

--- DotNetSelenium/SeleniumCustomMethods.cs
+++ DotNetSelenium/SeleniumCustomMethods.cs
@@ -5,36 +5,41 @@ namespace DotNetSelenium
 {
     internal class SeleniumCustomMethods
     {
-        public static void Click(IWebDriver driver, By locator)
+        public static void Click(IWebElement locator)
         {
-            driver.FindElement(locator).Click();
+            locator.Click();
         }
 
-        public static void EnterText(IWebDriver driver, By locator, string text)
+        public static void Submit(IWebElement locator)
         {
-            driver.FindElement(locator).Clear();
-            driver.FindElement(locator).SendKeys(text);
+            locator.Submit();
         }
 
-        public static void SelectDropdownByText(IWebDriver driver, By locator, string text)
+        public static void EnterText(IWebElement locator, string text)
         {
-            SelectElement dropdown = new SelectElement(driver.FindElement(locator));
+            locator.Clear();
+            locator.SendKeys(text);
+        }
+
+        public static void SelectDropdownByText(IWebElement locator, string text)
+        {
+            SelectElement dropdown = new SelectElement(locator);
             dropdown.SelectByText(text);
         }
 
-        public static void MultiSelectDropdown(IWebDriver driver, By locator, string[] values)
+        public static void MultiSelectDropdown(IWebElement locator, string[] values)
         {
-            SelectElement dropdown = new SelectElement(driver.FindElement(locator));
+            SelectElement dropdown = new SelectElement(locator);
             foreach (string value in values)
             {
                 dropdown.SelectByText(value);
             }
         }
 
-        public static List<String> GetSelectedItems(IWebDriver driver, By locator)
+        public static List<String> GetSelectedItems(IWebElement locator)
         {
             List<String> selectedItems = new List<String>();
-            SelectElement dropdown = new SelectElement(driver.FindElement(locator));
+            SelectElement dropdown = new SelectElement(locator);
             IList<IWebElement> selecteItems = dropdown.AllSelectedOptions;
             foreach (IWebElement item in selecteItems)
             {
