86e30c34725394b67b133a0df49da2ebf9300149
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2025-07-06 18:46:59 +0100

    Add login page

---
+++ DotNetSelenium/Pages/LoginPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+
+namespace DotNetSelenium.Pages
+{
+    internal class LoginPage
+    {
+        private readonly IWebDriver driver;
+
+        // Constructor to initialize the driver
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        // Locators (page elements)
+        IWebElement LoginLink => driver.FindElement(By.Id("loginLink"));
+        IWebElement UsernameField => driver.FindElement(By.Id("UserName"));
+        IWebElement PasswordField => driver.FindElement(By.Id("Password"));
+        IWebElement LoginButton => driver.FindElement(By.CssSelector(".btn"));
+
+        // Methods (actions)
+        public void ClickLoginLink()
+        {
+            LoginLink.Click();
+        }
+
+        public void Login(string username, string password)
+        {
+            UsernameField.Clear();
+            UsernameField.SendKeys(username);
+            PasswordField.Clear();
+            PasswordField.SendKeys(password);
+            LoginButton.Click();
+        }
+
+    }
+}
